fdca25cdd57dd47d9268556eddf5d804c9fa4f41
Lukas G <Lukas.Goetz@studium.uni-hamburg.de>
2014-12-07 12:59:02 +0100

    added marking of polymorphic vertices to test

--- src/test.c
+++ src/test.c
@@ -10,8 +10,13 @@
 
 /* adapted from SGA examples */
 #define MIN_CONTIG_LEN 200
+
 #define COPY_NUM_CUTOFF 0.3
 #define ASTAT_NUM_CUTOFF 20.0
+
+#define PROBABILITY_CUTOFF 0.01
+#define COPY_NUM_CUTOFF_2 1.5
+#define OVERLAP_CUTOFF 100
 
 int main(int argc, char **argv)
 {
@@ -46,11 +51,13 @@ int main(int argc, char **argv)
   gt_scaffolder_graph_test(5, 8, true, 5, true, 8, true);*/
 
   if (argc == 4) {
-    graph = NULL;
-    /* load contigs and distance information from file */
+    graph = NULL;    
     contig_filename = argv[1];
     dist_filename = argv[2];
     astat_filename = argv[3];
+
+    /* load contigs and distance information from file */
+    /* LG: return had_err by function ...new_from_file? */
     graph = gt_scaffolder_graph_new_from_file(contig_filename, MIN_CONTIG_LEN,
             dist_filename, err);
     gt_scaffolder_graph_print(graph, "gt_scaffolder_parser_test_complete.dot",
@@ -59,10 +66,17 @@ int main(int argc, char **argv)
     had_err = gt_scaffolder_graph_mark_repeats(astat_filename, graph,
               COPY_NUM_CUTOFF, ASTAT_NUM_CUTOFF, err);
 
-    if (had_err == 0)
+    if (had_err == 0) {
       gt_scaffolder_graph_print(graph,
             "gt_scaffolder_algorithms_test_filter_repeats.dot", err);
-    else
+      /* mark polymorphic vertices, edges and inconsistent edges */
+      had_err = gt_scaffolder_graph_filter(graph, PROBABILITY_CUTOFF,
+                COPY_NUM_CUTOFF_2, OVERLAP_CUTOFF);
+      if (had_err == 0)
+        gt_scaffolder_graph_print(graph,
+            "gt_scaffolder_algorithms_test_filter_polymorphism.dot", err);
+    }
+    if (had_err != 0)
       fprintf(stderr,"ERROR: %s\n",gt_error_get(err));
 
     gt_scaffolder_graph_delete(graph);
