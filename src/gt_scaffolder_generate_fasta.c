84e21e9c64600ce9f46b7d2c7eb50286f8757ea4
dorleosterode <9osterod@informatik.uni-hamburg.de>
2015-02-09 17:58:18 +0100

    changed licence year to 2015

--- src/gt_scaffolder_generate_fasta.c
+++ src/gt_scaffolder_generate_fasta.c
@@ -1,6 +1,6 @@
 /*
-  Copyright (c) 2014 Dorle Osterode, Stefan Dang, Lukas Götz
-  Copyright (c) 2014 Center for Bioinformatics, University of Hamburg
+  Copyright (c) 2015 Dorle Osterode, Stefan Dang, Lukas Götz
+  Copyright (c) 2015 Center for Bioinformatics, University of Hamburg
 
   Permission to use, copy, modify, and distribute this software for any
   purpose with or without fee is hereby granted, provided that the above
